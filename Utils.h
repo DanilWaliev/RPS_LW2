46c0010fffff03f27adf15b27f375da9371d229e
Danil Valiev <dnlvlv39@gmail.com>
2024-10-30 00:48:01 +0300

    feat: Utils module were created, string functions were defined

---
+++ Utils.h
@@ -0,0 +1,14 @@
+#pragma once
+
+using namespace std;
+
+#include <string>
+#include <locale>
+
+bool IsWord(string str);
+
+bool IsNumber(string str);
+
+string ToLowerCase(string str);
+
+string ToUpperCase(string str);
