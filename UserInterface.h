c70405df39cee9d3a519310577d177f544dabf9b
Danil Valiev <dnlvlv39@gmail.com>
2024-10-29 23:54:07 +0300

    feat: added UserInterface file, interface functions were declared

---
+++ UserInterface.h
@@ -0,0 +1,49 @@
+using namespace std;
+
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <fstream>
+
+enum class MenuOptions
+{
+	Exit,
+	FileInput,
+	ManualInput
+};
+
+class Menu
+{
+	MenuOptions userChoice;
+
+public:
+	static void Show(void);
+
+	void SetUserChoice(void);
+
+	MenuOptions GetUserChoice(void);
+};
+
+class InputHandler
+{
+public:
+	static int GetInt(
+		string promptMessage = "Введите целочисленное значение: ",
+		string errorMessage = "Некорректный ввод");
+
+	static string GetWord(
+		string promptMessage = "Введите строку: ",
+		string errorMessage = "Некорректный ввод");
+
+	static bool Prompt(std::string message);
+
+	static int GetVectorSize(void);
+
+	static void ManualFillVector(vector<int> nums);
+};
+
+class Printer
+{
+public:
+	static void PrintVector(vector<int> nums);
+};
