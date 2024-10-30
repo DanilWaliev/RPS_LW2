6e1ba388943f99c46604388eb9ccd2d63775db59
Danil Valiev <dnlvlv39@gmail.com>
2024-10-30 12:18:54 +0300

    feat: FileHandler class in FileUtils module were declared

--- FileUtils.h
+++ FileUtils.h
@@ -1,1 +1,14 @@
 using namespace std;
+
+#include <string>
+#include <filesystem>
+#include <ios>
+#include <vector>
+
+class FileHandler
+{
+public:
+	static bool IsFileNameReserved(string filename);
+
+	static void PrintVectorToFile(vector<int> nums);
+};
