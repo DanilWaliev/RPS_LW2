885a5e4ace8c41095bbffcffb7bbf7a4720df63d
Danil Valiev <dnlvlv39@gmail.com>
2024-11-02 17:23:18 +0300

    feat: input ways were defined

--- RPS_LW2.cpp
+++ RPS_LW2.cpp
@@ -5,6 +5,36 @@
 
 int main()
 {
+	// объект меню для работы с ним
+	Menu menu;
+	// вектор, в котором будем хранить числа
+	vector<int> nums;
+	
+	while (true)
+	{
+		// Вывод меню на экран
+		menu.Show();
+		// Получаем пункт меню, который выбрал пользователь
+		menu.SetUserChoice();
 
+		switch (menu.GetUserChoice())
+		{
+		case MenuOptions::ManualInput:
+			// Задаем размер вектора
+			nums.resize(VectorHandler::GetVectorSize());
+			// Ввод каждого элмента с клавиатуры
+			VectorHandler::ManualFillVector(nums);
+			break;
+		case MenuOptions::RandomInput:
+			nums.resize(VectorHandler::GetVectorSize());
+			VectorHandler::RandomFillVector(nums);
+
+			break;
+		case MenuOptions::Exit:
+			return EXIT_SUCCESS;
+		default:
+			cout << "Такого пункта в меню нет!" << endl;
+		}
+	}
 }
 
