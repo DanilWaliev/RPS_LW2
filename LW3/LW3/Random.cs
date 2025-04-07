fc4803affe0a49ed292a919463c41df7e50c98b1
Danil Valiev <dnlvlv39@gmail.com>
2025-04-07 03:15:58 +0300

    feat: random fill array was added

---
+++ LW3/LW3/Random.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LW3
+{
+    public class RandomUtils
+    {
+        public static int[] GetRandomArray(int maxLength)
+        {
+            Random random = new Random();
+
+            int elementLowerBound = -100;
+            int elementUpperBound = 100;
+
+            int[] arr = new int[random.Next(1, maxLength)];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(elementLowerBound, elementUpperBound);
+            }
+
+            return arr;
+        }
+    }
+}
